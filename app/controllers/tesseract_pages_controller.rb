c147f6d16d245956a4e2d3c22973a47a85cb3f2b
Fouad Yaseen <fouad2000@gmail.com>
2014-02-02 20:57:31 +0100

    testing command execution

--- app/controllers/tesseract_pages_controller.rb
+++ app/controllers/tesseract_pages_controller.rb
@@ -1,6 +1,13 @@
 class TesseractPagesController < ApplicationController
+  skip_before_filter :verify_authenticity_token 
   def run
   	puts "In Run Controller"
-   	render text: "testing"
+    puts params[:image]
+    jpg = Base64.decode64(params[:image])
+    File.open("testing.jpg",'wb') do |f|
+  		f.write jpg
+	end
+	str = %x(ls ~)
+   	render text: str
   end
 end
