a81e1de3d2724c4538eda5c9778fe9e1f262594d
Fouad Yaseen <fouad2000@gmail.com>
2014-02-03 17:15:05 +0100

    experiment

--- app/controllers/tesseract_pages_controller.rb
+++ app/controllers/tesseract_pages_controller.rb
@@ -4,13 +4,13 @@ class TesseractPagesController < ApplicationController
   	puts "In Run Controller"
     puts params[:image]
     jpg = Base64.decode64(params[:image])
-    File.open("ocr/sample.jpg",'wb') do |f|
-  		f.write jpg
-	end
+    #File.open("ocr/sample.jpg",'wb') do |f|
+  	#	f.write jpg
+	  #end
 	#str = %x(./ocr/bin/tesseract ocr/sample.jpg ocr/out)
   #str = %x(./ocr/bin/tesseract --version)
   %x(mkdir test123)
-  str = %x(dir test1*)
+  str = %x(ls)
    	render text: str
   end
 end
