00c96144949f32acba6ff41fde014231aa6e439c
Fouad Yaseen <fouad2000@gmail.com>
2014-02-03 23:43:12 +0100

    Added Tesseract support

--- app/controllers/tesseract_pages_controller.rb
+++ app/controllers/tesseract_pages_controller.rb
@@ -6,17 +6,17 @@ class TesseractPagesController < ApplicationController
 
     jpg = Base64.decode64(params[:image])
     
-    file = File.open("tmp/tessdir/sample.jpg",'wb')
+    file = File.open("tessdir/sample.jpg",'wb')
   	file.write jpg
 	  
     puts "Starting tesseract"
-    %x(tesseract tmp/tessdir/sample.jpg tmp/tessdir/out)
+    %x(tesseract tessdir/sample.jpg tessdir/out)
     puts "Done with tesseract"
   
-    file = File.open("tmp/tessdir/out.txt", "rb")
+    file = File.open("tessdir/out.txt", "rb")
     contents = file.read
     
-    %x(rm -Rf tmp/tessdir/*)
+    %x(rm -Rf tessdir/*)
     
     render text: contents
   end
