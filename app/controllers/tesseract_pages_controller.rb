05dc04067f62a027d854898e56404710ffe071be
Fouad Yaseen <fouad2000@gmail.com>
2014-02-03 23:34:58 +0100

    Added Tesseract support

--- app/controllers/tesseract_pages_controller.rb
+++ app/controllers/tesseract_pages_controller.rb
@@ -1,15 +1,21 @@
 class TesseractPagesController < ApplicationController
   skip_before_filter :verify_authenticity_token 
   def run
+
   	puts "In Run Controller"
-    puts params[:image]
+
     jpg = Base64.decode64(params[:image])
-    File.open("tmp/sample.jpg",'wb') do |f|
-  		f.write jpg
-	  end
-	#str = %x(./ocr/bin/tesseract ocr/sample.jpg ocr/out)
-  str = %x(tesseract --version)
-   	render text: "Done"
     
+    file = File.open("tmp/tessdir/sample.jpg",'wb')
+  	file.write jpg
+	  
+    %x(tesseract tmp/tessdir/sample.jpg tmp/tessdir/out)
+  
+    file = File.open("tmp/tessdir/out.txt", "rb")
+    contents = file.read
+    
+    %x(rm -Rf tmp/tessdir/*)
+    
+    render text: contents
   end
 end
